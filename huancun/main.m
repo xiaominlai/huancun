39d641bf863a3a0b38c42a1b64532c7b2744fb78
chenyehongfy <chenyehongfy@chenyehongfydeMacBook-Pro.local>
2014-10-07 09:47:25 +0800

    Initial Commit

---
+++ huancun/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  huancun
+//
+//  Created by chenyehongfy on 14-10-7.
+//  Copyright (c) 2014年 hongSheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
